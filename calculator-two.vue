efa9013eb1a21ea911cbe666047c9a817c670f74
Kyle Palermo <61397896+kylespalermo@users.noreply.github.com>
2024-07-05 10:51:36 -0400

    Initial rebuild of data model

---
+++ calculator-two.vue
@@ -0,0 +1,334 @@
+<script>
+//default value appears to be unecessary when using v-model
+export default {
+    data() {
+        const defaults = {
+            averageSalary: 80000,
+        }
+        return {
+            //company profile
+            //contact information
+            companyName: {
+                required: true,
+                value: ''
+            },
+
+            prospectiveCustomerEmail: {
+                required: true,
+                value: ''
+            },
+
+            fivetranContactEmail: {
+                required: true,
+                value: ''
+            },
+
+            //current staffing costs
+            averageSalary: {
+                required: true,
+                value: defaults.averageSalary,
+                increment: 1000,
+                annotation: 'thousands, $'
+            },
+
+            //work schedule
+            hoursWorkedPerWeek: {
+                required: true,
+                value: 40
+            },
+
+            weeksWorkedPerYear: {
+                required: true,
+                value: 48
+            },
+
+            //data integration cost and maintenance
+
+            numberOfEngineers: {
+                required: true,
+                value: 1,
+            },
+
+            pipelines: {
+                building: {
+                    currentState: {
+                        value: 0,
+                        annotation: 'Researching APIs, database documentation, code development.'
+                    },
+                    withFivetran: {
+                        value: 0,
+                        annotation: 'Adding new connectors in Fivetran.'
+                    }
+                },
+                uptimeMaintenance: {
+                    currentState: {
+                        value: 0,
+                        annotation: 'Updating API/Database versions, fixing bugs.'
+                    },
+                    withFivetran: {
+                        value: 0,
+                        annotation: 'Monitoring Fivetran pipeline health.'
+                    }
+                },
+                schemaChangeMaintenance: {
+                    currentState: {
+                        value: 0,
+                        annotation: 'Managing new field additions, deletions, data type changes.'
+                    },
+                    withFivetran: {
+                        value: 0,
+                        annotation: 'Fivetran automated schema migration.'
+                    }
+                },
+                deploymentAndOrchestration: {
+                    currentState: {
+                        value: 0,
+                        annotation: 'Promoting changes through dev/test, Co-ordinating pipeline runs.'
+                    },
+                    withFivetran: {
+                        value: 0,
+                        annotation: 'Fivetran API scheduler.'
+                    }
+                }
+            },
+            transformations: {
+                building: {
+                    currentState: {
+                        value: 0,
+                        annotation: 'Building transformations for new models.'
+                    },
+                    withFivetran: {
+                        value: 0,
+                        annotation: 'Building new models with Fivetran packages where possible.'
+                    }
+                },
+                modelMaintenance: {
+                    currentState: {
+                        value: 0,
+                        annotation: 'Updating transformations for existing models.'
+                    },
+                    withFivetran: {
+                        value: 0,
+                        annotation: 'Fivetran managed packages - you just need to maintain custom code.'
+                    }
+                },
+                deploymentAndOrchestration: {
+                    currentState: {
+                        value: 0,
+                        annotation: 'Promoting changes through dev/test, Co-ordinating transformation runs.',
+                        increment: 1000
+                    },
+                    withFivetran: {
+                        value: 0,
+                        annotation: 'Fivetran transformations scheduler.',
+                        increment: 1000
+                    }
+                }
+            },
+            infrastructureCosts: {
+                annotation: 'annual, $',
+                currentState: {
+                    value: 0,
+                    annotation: 'VMs/networking/scheduler costs for running pipelines/transformations.',
+                    increment: 1000
+                },
+                withFivetran: {
+                    value: 0,
+                    annotation: 'Reduced costs with Fivetran.',
+                    increment: 1000
+                }
+            },
+            dataUptime: {
+                currentState: {
+                    value: 99.9,
+                    annotation: 'Costs associated with Data Downtime e.g. lost orders from data pipeline bugs.',
+                    increment: .1
+                },
+                withFivetran: {
+                    default: 99.99,
+                    annotation: 'Reduced downtime with Fivetran',
+                    increment: .1
+                }
+            },
+            destinationIngestionCosts: {
+                annotation: 'annual, $',
+                currentState: {
+                    value: '',
+                    annotation: 'Data warehouse/lake ingestion costs with current solution.',
+                    increment: 1000
+                },
+                withFivetran: {
+                    value: '',
+                    annotation: 'Reduced deestination ingestion costs with Fivetran',
+                    increment: 1000
+                }
+            },
+            costOfSolution: {
+                annotation: 'annual, $',
+                currentState: {
+                    value: '',
+                    annotation: 'Cost of alternative/incumbent solution.',
+                    increment: 1000
+                },
+                withFivetran: {
+                    value: '',
+                    annotation: 'Cost of Fivetran',
+                    increment: 1000
+                }
+            },
+
+            costOfDataDowntimePerHour: {
+                value: '',
+                annotation: '$',
+                increment: 1000,
+            },
+
+            additionalCosts: {
+                value: '',
+                annotation: 'annual, $',
+                increment: 1000
+            },
+
+            idcWorkFunctions: {
+                dataEngineers: {
+                    customSalarySet: { value: false },
+                    equivalentProductivityLevel: { value: 2 },
+                    salary: { value: 70000 },
+                    productivityGain: { value: 0.48 },
+                    include: { value: false },
+                },
+                dataAnalytics: {
+                    customSalarySet: { value: false },
+                    equivalentProductivityLevel: { value: 0 },
+                    salary: { value: 70000 },
+                    productivityGain: { value: 0.02 },
+                    include: { value: false },
+                },
+                dataGovernance: {
+                    customSalarySet: { value: false },
+                    equivalentProductivityLevel: { value: 0 },
+                    salary: { value: 70000 },
+                    productivityGain: { value: 0.16 },
+                    include: { value: false },
+                },
+                finance: {
+                    customSalarySet: { value: false },
+                    equivalentProductivityLevel: { value: 0 },
+                    salary: { value: 70000 },
+                    productivityGain: { value: 0.40 },
+                    include: { value: false },
+                },
+                productManagement: {
+                    customSalarySet: { value: false },
+                    equivalentProductivityLevel: { value: 0 },
+                    salary: { value: 70000 },
+                    productivityGain: { value: 0.12 },
+                    include: { value: false },
+                },
+                salesAndCustomerSuccess: {
+                    customSalarySet: { value: false },
+                    equivalentProductivityLevel: { value: 0 },
+                    salary: { value: 70000 },
+                    productivityGain: { value: 0.19 },
+                    include: { value: false },
+                },
+                logistics: {
+                    customSalarySet: { value: false },
+                    equivalentProductivityLevel: { value: 0 },
+                    salary: { value: 70000 },
+                    productivityGain: { value: 0.29 },
+                    include: { value: false },
+                },
+                marketing: {
+                    customSalarySet: { value: false },
+                    equivalentProductivityLevel: { value: 0 },
+                    salary: { value: 70000 },
+                    productivityGain: { value: 0.16 },
+                    include: { value: false },
+                }
+            },
+
+            revenueGains: {
+                customSalarySet: { value: false },
+                additionalGrossRevenue: { value: 554894 },
+                margin: { value: .15 },
+                include: { value: false },
+                annualNetGain: { value: this.additionalGrossRevenue * this.margin },
+            },
+
+            operationalCostSavings: {
+                customSalarySet: { value: false },
+                oneTime: { value: 285714 },
+                annual: { value: 177400 },
+                include: { value: false },
+                averageAnnualSaving: { value: this.oneTime + this.annual }
+            }
+        }
+    },
+    computed: {
+        annualNetGain() {
+            return this.revenueGains.additionalGrossRevenue.value * this.revenueGains.margin.value;
+        },
+        averageAnnualSaving() {
+            return this.operationalCostSavings.oneTime.value + this.operationalCostSavings.annual.value
+        },
+        equivalentHourlyRate() {
+            return this.averageSalary.value / (this.weeksWorkedPerYear.value * this.hoursWorkedPerWeek.value)
+        },
+        idcProductivityGains(){
+            let idcProductivityGains = {};
+            for (const key in this.idcWorkFunctions) {
+                if (this.idcWorkFunctions[key].equivalentProductivityLevel.value > 0) {
+                    idcProductivityGains[key] = this.calculateIDCGains(this.idcWorkFunctions[key].salary.value, this.idcWorkFunctions[key].equivalentProductivityLevel.value, this.idcWorkFunctions[key].productivityGain.value)
+                }
+            }
+            return idcProductivityGains;
+        },
+        totalTime() {
+            let pipelines = Object.values(this.pipelines);
+            let transformations = Object.values(this.transformations);
+
+            console.log(pipelines)
+
+            let totalCurrentPipelineHours = pipelines.reduce((total, obj) => obj.currentState.value + total, 0);
+            let totalFivetranPipelineHours = pipelines.reduce((total, obj) => obj.withFivetran.value + total, 0);
+
+            let totalCurrentTransformationsHours = transformations.reduce((total, obj) => obj.currentState.value + total, 0);
+            let totalFivetranTransformationsHours = transformations.reduce((total, obj) => obj.withFivetran.value + total, 0);
+
+            console.log(totalCurrentPipelineHours)
+
+            let currentState = totalCurrentPipelineHours + totalCurrentTransformationsHours;
+            let withFivetran = totalFivetranPipelineHours + totalFivetranTransformationsHours;
+
+            return {
+                currentState: currentState,
+                withFivetran: withFivetran
+            }
+        }
+    },
+    methods: {
+        calculateIDCGains(salary, equivalentProductivityLevel, productivityGain) {
+            let currentState = salary * equivalentProductivityLevel;
+            let withFivetran = (currentState * productivityGain) + currentState;
+            let averageAnnualGain = this.calculateCostSavings(withFivetran, currentState);
+            return {
+                currentState: currentState,
+                withFivetran: withFivetran,
+                averageAnnualGain: averageAnnualGain
+            }
+        },
+        calculateCostSavings(withFivetran, currentState) {
+            return withFivetran - currentState
+        },
+        updateIDC() {
+            for(idcWorkFunctions of idcWorkFunctions) {
+                if (!idcWorkFunction.customSalarySet) {
+                    this.idcWorkFunction.salary.value = this.averageSalary;
+                }
+            }
+        }
+    },
+}
+
+</script>
